e4ba13b0623d3ee300291bd13dba0bda53c25651
Max <Max@18.111.100.49>
2011-01-04 22:59:21 -0500

    build start recyclers

--- teams/examplefuncsplayer/RobotPlayer.java
+++ teams/examplefuncsplayer/RobotPlayer.java
@@ -1,8 +1,21 @@
 package examplefuncsplayer;
 //adding a comment to test git
 //new comment!
-import battlecode.common.*;
-import static battlecode.common.GameConstants.*;
+import java.util.ArrayList;
+
+import battlecode.common.BuilderController;
+import battlecode.common.Chassis;
+import battlecode.common.ComponentController;
+import battlecode.common.ComponentType;
+import battlecode.common.Direction;
+import battlecode.common.MapLocation;
+import battlecode.common.Mine;
+import battlecode.common.MovementController;
+import battlecode.common.Robot;
+import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+import battlecode.common.RobotLevel;
+import battlecode.common.SensorController;
 
 public class RobotPlayer implements Runnable {
 
@@ -26,9 +39,12 @@ public class RobotPlayer implements Runnable {
 		//System.out.println(java.util.Arrays.toString(components));
 		//System.out.flush();
 		if(myRC.getChassis()==Chassis.BUILDING){
+			while(true){
+				myRC.yield();
+			}
 			//System.out.println(components[1].componentClass()+ " length "+components.length);
-			myRC.setIndicatorString(0,components[1].componentClass()+ " length "+components.length);
-			runBuilder((MovementController)components[0],(BuilderController)components[2]);
+			//myRC.setIndicatorString(0,components[1].componentClass()+ " length "+components.length);
+			//runBuilder((MovementController)components[0],(BuilderController)components[2]);
 		}
 		else
 			runMotor((MovementController)components[0]);
@@ -42,26 +58,29 @@ public class RobotPlayer implements Runnable {
 	
 		while (true) {
             try {
-
-				myRC.yield();
-
-				if(!motor.canMove(myRC.getDirection()))
-					motor.setDirection(myRC.getDirection().rotateRight());
-				else if(myRC.getTeamResources()>=2*Chassis.LIGHT.cost)
-					for(int i=1; i<testBuilding.length; i++){
-						try{
-							builder.build(testBuilding[i],myRC.getLocation().add(myRC.getDirection()),RobotLevel.ON_GROUND);
-							System.out.println(testBuilding[i].name());
-							myRC.yield();
-						}catch(IllegalArgumentException e){
-							System.out.println("catching an error");
-							myRC.yield();
-						}catch(GameActionException e){
-							System.out.println(testBuilding[i].name());
-							myRC.yield();
-						}
-					}
-				//builder.build(Chassis.LIGHT,myRC.getLocation().add(myRC.getDirection()));
+            	
+            	
+            	
+//				myRC.yield();
+//
+//				if(!motor.canMove(myRC.getDirection()))
+//					motor.setDirection(myRC.getDirection().rotateRight());
+//				else if(myRC.getTeamResources()>=2*Chassis.LIGHT.cost)
+//					for(int i=1; i<testBuilding.length; i++){
+//						try{
+//							builder.build(testBuilding[i],myRC.getLocation().add(myRC.getDirection()),RobotLevel.ON_GROUND);
+//							System.out.println(testBuilding[i].name());
+//							myRC.yield();
+//						}catch(IllegalArgumentException e){
+//							System.out.println("catching an error");
+//							myRC.yield();
+//						}catch(GameActionException e){
+//							System.out.println(testBuilding[i].name());
+//							myRC.yield();
+//						}
+//					}
+//				//builder.build(Chassis.LIGHT,myRC.getLocation().add(myRC.getDirection()));
+//				
             } catch (Exception e) {
                 System.out.println("caught exception:");
                 e.printStackTrace();
@@ -74,16 +93,106 @@ public class RobotPlayer implements Runnable {
         while (true) {
             try {
                 /*** beginning of main loop ***/
+            	
                 while (motor.isActive()) {
                     myRC.yield();
                 }
-                
-                if (motor.canMove(myRC.getDirection())) {
-                    //System.out.println("about to move");
-                    motor.moveForward();
-                } else {
-                    motor.setDirection(myRC.getDirection().rotateRight());
-                }
+                //determine components
+                ComponentController [] c = myRC.components();
+            	SensorController sight = null;
+            	BuilderController constructor = null;
+            	for(int i=0; i<c.length; i++){
+            		if (c[i].type()==ComponentType.CONSTRUCTOR){
+            			constructor = (BuilderController) c[i];
+            		}else if (c[i].type()==ComponentType.SIGHT){
+            			sight = (SensorController) c[i];
+            		}
+            	}
+            	
+            	//bug-pathing code
+            	//Robot [] nearbyRobots = (sight).senseNearbyGameObjects(Robot.class);
+        		//int numberNearby = nearbyRobots.length;
+        		//if (!motor.isActive()){
+        		//	bug(motor, sight,numberNearby);
+            	//}
+        		
+            	Mine [] nearbyMines = sight.senseNearbyGameObjects(Mine.class);
+            	//ArrayList<MapLocation> finalMines = new ArrayList<MapLocation>();
+            	MapLocation closestMine = null;
+            	int closestDist = 10000;
+            	int challengerDist=  10000;
+            	//remove the mines that are taken
+            	for(int i=0; i<nearbyMines.length; i++){
+            		MapLocation pos = sight.senseLocationOf(nearbyMines[i]);
+            		Robot guyOnTheMine = null;
+            		guyOnTheMine = (Robot) sight.senseObjectAtLocation(pos, RobotLevel.ON_GROUND);
+            		if (guyOnTheMine!=null){
+            			RobotInfo info = sight.senseRobotInfo(guyOnTheMine);
+                		if (info.chassis!=Chassis.BUILDING){//someone is there but not a building
+                			challengerDist = myRC.getLocation().distanceSquaredTo(pos);
+                			if (challengerDist<closestDist){
+                				closestDist = challengerDist;
+                				closestMine = pos;
+                			}
+                		}
+            		}else{//no one on the mine
+            			challengerDist = myRC.getLocation().distanceSquaredTo(pos);
+            			if (challengerDist<closestDist){
+            				closestDist = challengerDist;
+            				closestMine = pos;
+            			}
+            		}
+            	}
+            	//TODO go to closest, not the first in the list
+            	int dist = 10000;
+            	MapLocation goal = null;
+            	if (closestMine!=null){
+            		goal = closestMine;
+            		dist = closestDist;
+            	}
+
+        		if (closestMine==null){//no mines; keep looking
+        			motor.setDirection(myRC.getDirection().rotateRight().rotateRight());
+    				myRC.setIndicatorString(1, "Searching");
+        		}else if (dist<=(constructor.type().range)){//within range to build
+        			Direction goaldir = myRC.getLocation().directionTo(goal);
+        			if (myRC.getDirection()!=goaldir){
+        				motor.setDirection(goaldir);
+        				myRC.yield();
+        			}
+        			if (myRC.getTeamResources()>(Chassis.BUILDING.cost+ComponentType.RECYCLER.cost)){
+        				constructor.build(Chassis.BUILDING, goal);
+        				myRC.yield();
+        				constructor.build(ComponentType.RECYCLER, goal,RobotLevel.ON_GROUND);
+        			}else{
+        				myRC.setIndicatorString(1, "Insufficient $");
+        			}
+        		}else{//need to walk there.
+    				myRC.setIndicatorString(1, "Need to travel to target");
+        			Direction goaldir = myRC.getLocation().directionTo(goal);//TODO this rotation may prevent you from walking on the goal
+        			if (myRC.getDirection()==goaldir){//Correct direction
+        				while (!motor.canMove(myRC.getDirection())){
+            				motor.setDirection(myRC.getDirection().rotateRight());
+            				myRC.setIndicatorString(1, "Avoiding obstacle");
+            				myRC.yield();
+        				}
+        				motor.moveForward();
+        				myRC.setIndicatorString(1, "Traveling to target");
+        			}else{
+        				motor.setDirection(goaldir);
+        			}
+        		}
+            	
+        		myRC.yield();
+            	
+            	
+//                
+//                if (motor.canMove(myRC.getDirection())) {
+//                    //System.out.println("about to move");
+//                    motor.moveForward();
+//                } else {
+//                    motor.setDirection(myRC.getDirection().rotateRight());
+//                }
 
                 /*** end of main loop ***/
             } catch (Exception e) {
--- teams/guns/RobotPlayer.java
+++ teams/guns/RobotPlayer.java
@@ -1,6 +1,8 @@
 package guns;
 //adding a comment to test git
 //new comment!
+import java.awt.Component;
+
 import battlecode.common.*;
 import static battlecode.common.GameConstants.*;
 
@@ -27,7 +29,7 @@ public class RobotPlayer implements Runnable {
 		//System.out.flush();
 		if(myRC.getChassis()==Chassis.BUILDING){
 			//System.out.println(components[1].componentClass()+ " length "+components.length);
-			myRC.setIndicatorString(0,components[1].componentClass()+ " length "+components.length);
+			//myRC.setIndicatorString(0,components[1].componentClass()+ " length "+components.length);
 			runBuilder((MovementController)components[0],(BuilderController)components[2]);
 		}
 		else
@@ -39,32 +41,46 @@ public class RobotPlayer implements Runnable {
 	}
 
 	public void runBuilder(MovementController motor, BuilderController builder) {
-	
+		//get the components
+		ComponentController [] c = myRC.components();
+    	SensorController sight = null;
+    	BuilderController constructor = null;
+    	for(int i=0; i<c.length; i++){
+    		if (c[i].type()==ComponentType.RECYCLER){
+    			constructor = (BuilderController) c[i];
+    		}else if (c[i].type()==ComponentType.BUILDING_SENSOR){
+    			sight = (SensorController) c[i];
+    		}
+    	}
+    	
+    	
 		while (true) {
             try {
 
 				myRC.yield();
-
+				
+				//build light chassises
 				if(!motor.canMove(myRC.getDirection()))
 					motor.setDirection(myRC.getDirection().rotateRight());
 				else if(myRC.getTeamResources()>=2*Chassis.LIGHT.cost)
-					for(int i=1; i<testBuilding.length; i++){
-						try{
-							builder.build(testBuilding[i],myRC.getLocation().add(myRC.getDirection()),RobotLevel.ON_GROUND);
-							System.out.println(testBuilding[i].name());
-							myRC.yield();
-						}catch(IllegalArgumentException e){
-							System.out.println("catching an error");
-							myRC.yield();
-						}catch(GameActionException e){
-							System.out.println(testBuilding[i].name());
-							myRC.yield();
-						}
-					}
-				//builder.build(Chassis.LIGHT,myRC.getLocation().add(myRC.getDirection()));
+					//for(int i=1; i<testBuilding.length; i++){
+					builder.build(Chassis.LIGHT,myRC.getLocation().add(myRC.getDirection()));
+				//System.out.println(testBuilding[i].name());
+				
+				//try to equip constructors
+				Robot [] nearbyRobots = (sight).senseNearbyGameObjects(Robot.class);
+				int numberNearby = nearbyRobots.length;
+				if (numberNearby>0){//Try to build guns!
+					//MapLocation target = sight.senseLocationOf(nearbyRobots[0]);
+					MapLocation target = myRC.getLocation().add(myRC.getDirection());
+					tryToAttach(target,ComponentType.CONSTRUCTOR,constructor);
+				}else{
+					myRC.setIndicatorString(0,"not trying");
+				}
+				
             } catch (Exception e) {
-                System.out.println("caught exception:");
-                e.printStackTrace();
+            	System.out.println("caught exception:");
+            	e.printStackTrace();
             }
         }
 	}
@@ -74,16 +90,49 @@ public class RobotPlayer implements Runnable {
         while (true) {
             try {
                 /*** beginning of main loop ***/
-                while (motor.isActive()) {
-                    myRC.yield();
-                }
+
+            	ComponentController [] c = myRC.components();
+            	SensorController sight = null;
+            	BuilderController constructor = null;
+            	for(int i=0; i<c.length; i++){
+            		if (c[i].type()==ComponentType.CONSTRUCTOR){
+            			constructor = (BuilderController) c[i];
+            		}else if (c[i].type()==ComponentType.SIGHT){
+            			sight = (SensorController) c[i];
+            		}
+            	}
+            	
+            	if ((sight) != null){
+            		//myRC.setIndicatorString(0," "+c[0].type());
+            		//myRC.setIndicatorString(1," "+c[1].type());
+            		//myRC.setIndicatorString(2," "+c[2].type());
+            		Robot [] nearbyRobots = (sight).senseNearbyGameObjects(Robot.class);
+            		int numberNearby = nearbyRobots.length;
+            		if (!motor.isActive()){
+            			bug(motor, sight,numberNearby);
+                	}
+            		
+            		if (numberNearby>0){//Try to build guns!
+            			//tryToAttach(sight.senseLocationOf(nearbyRobots[0]),ComponentType.CONSTRUCTOR,constructor);
+            		}else{
+            			myRC.setIndicatorString(0,"not trying");
+            		}
+            	}
+            	myRC.yield();
+
+            	
+            	
+            	
+//                while (motor.isActive()) {
+//                    myRC.yield();
+//                }
                 
-                if (motor.canMove(myRC.getDirection())) {
-                    //System.out.println("about to move");
-                    motor.moveForward();
-                } else {
-                    motor.setDirection(myRC.getDirection().rotateRight());
-                }
+//                if (motor.canMove(myRC.getDirection())) {
+//                    //System.out.println("about to move");
+//                    motor.moveForward();
+//                } else {
+//                    motor.setDirection(myRC.getDirection().rotateRight());
+//                }
 
                 /*** end of main loop ***/
             } catch (Exception e) {
@@ -92,4 +141,31 @@ public class RobotPlayer implements Runnable {
             }
         }
     }
+
+	private void tryToAttach(MapLocation alliedLoc,
+			ComponentType comp,BuilderController constructor) throws GameActionException {
+
+		Boolean enoughMoney = myRC.getTeamResources()>=1.1*ComponentType.CONSTRUCTOR.cost;
+		//Boolean withinRange = constructor.withinRange(alliedLoc);
+		int dist = myRC.getLocation().distanceSquaredTo(alliedLoc);
+		Boolean withinRange = dist<=1;
+		myRC.setIndicatorString(0,"try constr. "+withinRange+"far "+dist +" "+enoughMoney);
+		
+		if (withinRange && enoughMoney){
+			constructor.build(comp, alliedLoc,RobotLevel.ON_GROUND);
+			myRC.setIndicatorString(2,"success!");
+		}
+		
+	}
+
+	private void bug(MovementController motor, SensorController sight, int numberNearby) throws GameActionException {
+		myRC.setIndicatorString(1," "+ numberNearby+" robots sensed");
+		if (numberNearby==0){
+			motor.setDirection(myRC.getDirection().rotateRight());
+		}else if (motor.canMove(myRC.getDirection())){
+			motor.moveForward();
+		}else{
+			motor.setDirection(myRC.getDirection().rotateRight());
+		}
+	}
 }
